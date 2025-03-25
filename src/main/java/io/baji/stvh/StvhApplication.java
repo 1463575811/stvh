6edcb40523b01c13505b0017cf85bd087f4a93ef
baji <1463575811@qq.com>
2025-03-25 22:05:16 +0800

    1

---
+++ src/main/java/io/baji/stvh/StvhApplication.java
@@ -0,0 +1,13 @@
+package io.baji.stvh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StvhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StvhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/baji/stvh/StvhApplicationTests.java
@@ -0,0 +1,13 @@
+package io.baji.stvh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StvhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
